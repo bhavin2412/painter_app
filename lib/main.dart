9983049503387560ba4f3b875fed4c9e08aaf869
Bhavin <bhavin2412>
2023-08-14 21:40:49 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:painter_app/painter/view/painter_homescreen.dart';
+import 'package:painter_app/painter/view/splash_screen.dart';
+import 'package:sizer/sizer.dart';
+
+void main()
+{
+  runApp(
+    Sizer(
+      builder: (context, orientation, deviceType) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: "splash",
+        routes: {
+          "/":(context) => PainterHome(),
+          "splash":(context) => SplashScreen(),
+        },
+      ),
+    ),
+  );
+}
